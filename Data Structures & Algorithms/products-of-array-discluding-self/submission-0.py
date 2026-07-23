09fb71cd4d01f75e1f001edc91251e4c2c871085
Jianing Li <jianinglive353@gmail.com>
2026-07-23 16:08:42 +0100

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if (i == j):
+                    continue
+                res[i] = res[i] * nums[j]
+
+        return res
